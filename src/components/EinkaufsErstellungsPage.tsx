7ed8df3588518dff7bbbf3cb25128904ebe58b51
Selina Edelsbrunner <edesec20@htl-kaindorf.at>
2024-06-07 09:23:52 +0200

    Bekanntgabe.tsx

--- src/components/EinkaufsErstellungsPage.tsx
+++ src/components/EinkaufsErstellungsPage.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 const EinkaufsErstellungsPage = () => {
+    const navigate = useNavigate();
+
+    const onhandleBekanntgabe = () => {
+        navigate("/createEinkauf")
+    }
     return (
-        <div>
-            Test
-        </div>
+        <>
+            <Button type="submit" variant="contained" color="primary" fullWidth onClick={() => onhandleBekanntgabe()}>
+                Einkauf erstellen
+            </Button>
+        </>
     );
 };
 
